814ac4c0ab2f0e83c5eb7b1e1a95683273fc6c22
Chaitanya <chaitanyakhandekar95@gmail.com>
2025-08-13 18:06:20 +0530

    isVerified user endpoint added

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -2,7 +2,8 @@ import e from 'express';
 import Router from 'express';
 import {
      verifyUser,
-     registerUser
+     registerUser,
+     isVerifiedUser
 
  } from '../controllers/user.controller.js';
  import { sendVerificationToken } from '../services/sendVerificationToken.js';
@@ -11,5 +12,6 @@ const router = Router();
 
 router.route("/register").post(registerUser, sendVerificationToken)
 router.route("/email/verify/:token").get(verifyUser)
+router.route("/email/verify/:email").get(isVerifiedUser)
 
 export default router;
