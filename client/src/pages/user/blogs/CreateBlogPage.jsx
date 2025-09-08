ba1255337d6b2b26487ea0d168a86d2dff3d2646
Chaitanya <chaitanyakhandekar95@gmail.com>
2025-09-08 19:08:30 +0530

    blogCreation page for mobile is design ready

--- client/src/pages/user/blogs/CreateBlogPage.jsx
+++ client/src/pages/user/blogs/CreateBlogPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import { 
   Save, 
   Eye, 
@@ -23,13 +23,34 @@ import {
   X,
   Plus
 } from 'lucide-react';
+import MDEditor from "@uiw/react-md-editor"
+import rehypeHighlights from "rehype-highlight"
+
 
 function CreateBlogPage() {
+
+    const [content,setContent] = useState("")
 
     useEffect(() => {
         const html = document.documentElement
         html.classList.add("dark")
     }, [])
+
+    const categories = [
+    'Frontend Development',
+    'Backend Development', 
+    'Full Stack',
+    'Mobile Development',
+    'DevOps',
+    'Data Science',
+    'Machine Learning',
+    'Cybersecurity',
+    'Web3/Blockchain',
+    'Career & Growth',
+    'Tutorials',
+    'Opinion & Discussion'
+  ];
+
     return (
         <div className="min-w-screen min-h-screen dark:bg-[#111826]">
 
@@ -45,12 +66,12 @@ function CreateBlogPage() {
                 </div>
 
                 <div className="h-full text-black dark:text-white flex gap-10 text-sm">
-                    <div className="h-full flex gap-2 items-center text-gray-400">
+                    <div className="hidden md:block h-full md:flex md:gap-2 md:items-center text-gray-400">
                         <Clock size={15}/>
                         <p>0 words . 0 min read</p>
                     </div>
 
-                    <div className="h-full flex items-center gap-5 text-gray-300">
+                    <div className="hidden md:block h-full md:flex md:items-center md:gap-5 text-gray-300">
                         <div className='p-2 hover:bg-gray-600 hover:p-2 hover:rounded-md transition-all duration-5'>
                             <Eye size={22}/>
                         </div>
@@ -66,6 +87,82 @@ function CreateBlogPage() {
                     </div>
                 </div>
             </nav>
+
+            <div className=" w-screen flex flex-col items-center gap-3 py-2 md:flex-row">
+
+                <div className=" w-[95%]">
+                    <MDEditor
+                        value={content}
+                        onChange={setContent}
+                        preview="live"
+                        height={750}
+                        className='dark:bg-[#1f2936] dark:rounded-md'
+                        previewOptions={{
+                            rehypePlugins:[[rehypeHighlights]]
+                        }}
+                    />
+                </div>
+
+                <div className=" w-[95%] flex flex flex-col gap-5 pb-7">
+                    <div className="border-[0.2px] border-gray-700 flex flex-col gap-3 items-start rounded-md w-full dark:bg-[#1f2936] p-5">
+                        <h1 className="text-md text-white font-bold">Publish Settings</h1>
+                        <div className="w-full flex flex-col gap-2 text-gray-300">
+                            <label>Status</label>
+
+                            <select className='dark:bg-[#374150] border-none outline-none p-2 rounded-md'>
+                                <option>Draft</option>
+                                <option>Published</option>
+                                <option>Private</option>
+                            </select>
+                        </div>
+
+                        <div className="w-full flex flex-col gap-2 text-gray-300">
+                            <label>Category</label>
+
+                            <select className='dark:bg-[#374150] border-none outline-none p-2 rounded-md'>
+
+                                {
+                                    categories.map((category,index)=>(
+                                        <option key={index}>{category}</option>
+                                    ))
+                                }
+
+                            </select>
+                        </div>
+
+                    </div>
+
+                    <div className="w-full flex flex-col gap-5 dark:bg-[#1f2936] p-5 rounded-md">
+                            <h1 className="text-white text-md font-bold">Tags</h1>
+                            <div className="flex w-full gap-3 items-center ">
+                                 <input type="text" className='w-full dark:bg-[#374150] dark:text-white border-none outline-none p-2 rounded-md' placeholder='Add a tag' />
+                            <div className="bg-gradient-to-r from-[#4777f4] to-[#9035ea] p-2 w-10 h-10 flex justify-center items-center rounded-md">
+                                <Plus className="text-white" size={19}/>
+                            </div>
+                            </div>
+
+                            <p className='dark:text-gray-300 text-sm md:text-md'>0/5 tags used</p>
+                    </div>
+
+                    <div className="w-full flex flex-col gap-5 dark:bg-[#1f2936] p-5 rounded-md">
+                            <h1 className="text-white text-md font-bold">Statistics</h1>
+                            <div className="flex justify-between w-full">
+                                <p className='dark:text-gray-300 text-sm md:text-md'>Words:</p>
+                                <p className='dark:text-gray-300 text-sm md:text-md'>0</p>
+                            </div>
+                            <div className="flex justify-between w-full">
+                                <p className='dark:text-gray-300 text-sm md:text-md'>Characters:</p>
+                                <p className='dark:text-gray-300 text-sm md:text-md'>0</p>
+                            </div>
+                            <div className="flex justify-between w-full">
+                                <p className='dark:text-gray-300 text-sm md:text-md'>Reading Time:</p>
+                                <p className='dark:text-gray-300 text-sm md:text-md'>0 min</p>
+                            </div>
+                    </div>
+
+
+                </div>
+            </div>
 
         </div>
     )
