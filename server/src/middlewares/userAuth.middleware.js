ca37364aa8a7078a65c571e404ebcf5c2b149f38
Chaitanya <chaitanyakhandekar95@gmail.com>
2025-09-10 19:44:20 +0530

    createBlog service completed for mvp

--- server/src/middlewares/userAuth.middleware.js
+++ server/src/middlewares/userAuth.middleware.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiUtils.js";
 import jwt from "jsonwebtoken";
-import { User } from "../models/user.model.js";
+import { User } from "../models/users.model.js";
 import dotenv from "dotenv"
 
 dotenv.config({path:"./.env"})
